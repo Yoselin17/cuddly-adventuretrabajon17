cb22edb114718de36e9d7f87883ef913eea3503b
yoselin retamal <yoselin_yar@hotmail.com>
2022-05-20 20:06:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+
+const App = () => {
+  const [frases, setFrases] = useState([]);
+  const [frase, setFrase] = useState("");
+
+
+
+
+  const eliminarfrases = indexfrases => {
+    let newArray = frases.filter((item, indice) => indice !== indexfrases);
+
+    setFrases(newArray)
+
+  }
+
+
+
+
+
+  const agregarFrases = event => {
+
+
+    setFrase(event.target.value);
+    console.log(frase);
+  }
+
+  function Agregaraqui(e) {
+    console.log(Agregaraqui)
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        onSubmit={evento => {
+          setFrases(frases.concat(frase));
+          evento.preventDefault();
+          setFrase("");
+        }}>
+
+        <input type="text" name="datos" value={frase} onChange={evento => { setFrase(evento.target.value) }} />
+
+      </form>
+
+      {frases.map((frase, index) => {
+        return (
+          <ul
+            key={index}>
+            {frase}
+            <button onClick={()=>{eliminarfrases(index)}}>
+              Anular
+            </button>
+
+          </ul>
+
+        );
+      })}
+
+
+
     </div>
+
   );
 }
 
