92fff0d6881c93bff4812e4c593885cf55dac852
yoselin retamal <yoselin_yar@hotmail.com>
2022-06-03 14:12:29 -0400

    parte del trabajo.

--- src/App.js
+++ src/App.js
@@ -8,26 +8,208 @@ const App = () => {
   const [frases, setFrases] = useState([]);
   const [frase, setFrase] = useState("");
 
+  useEffect(() => {
+    getFrases();
+  }, []);
 
+  useEffect(() => {
+    agregarput();
+    //agregar funcion nueva (eliminar)
+  }, [frases]);
+
+
+
+async function getFrases () {
+  const resp = await fetch('https://assets.breatheco.de/apis/fake/todos/user/usernameyoselin', {
+      method: "GET",
+
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then(data => {
+        let html = data.map(tarea => tarea.label);
+        console.log(data);
+        console.log(html);
+        setFrases(html)
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        //esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        //manejo de errores
+        console.log(error);
+      });
+
+
+
+  }
 
 
   const eliminarfrases = indexfrases => {
     let newArray = frases.filter((item, indice) => indice !== indexfrases);
 
-    setFrases(newArray)
+    setFrases(newArray);
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/usernameyoselin', {
+      method: "PUT",
+      body: JSON.stringify(
+        frases.map(item => {
+          return {
+            label: item, done: true
+          };
+        })
+
+      ),
+
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then(data => {
+
+        console.log(data);
+
+
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        //esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        //manejo de errores
+        console.log(error);
+      });
 
   }
-
-
-
 
 
   const agregarFrases = event => {
 
-
     setFrase(event.target.value);
-    console.log(frase);
+    console.log(frases)
+    /* fetch('https://assets.breatheco.de/apis/fake/todos/user/usernameyoselin', {
+      method: "PUT",
+      body: JSON.stringify(
+        frases.map(item => {
+          return {
+            label: item, done: false
+          }
+        })
+
+      ),
+
+
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then(data => {
+
+        console.log(data);
+
+
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        //esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        //manejo de errores
+        console.log(error);
+      }); */
+
+
+
+
   }
+
+  const eliminarTodo = () => {
+    setFrases([]);
+
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/usernameyoselin', {
+      method: "DELETE",
+      body: JSON.stringify(),
+      headers: {
+        "Content-Type": "application/json"
+
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then(data => {
+        setFrases([])
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        //esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        //manejo de errores
+        console.log(error);
+      });
+
+
+
+  }
+
+
+const agregarput = () => {
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/usernameyoselin', {
+      method: "PUT",
+      body: JSON.stringify(
+        frases.map(item => {
+          return {
+            label: item, done: false
+          }
+        })
+
+      ),
+
+
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then(data => {
+
+        console.log(data);
+
+
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        //esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        //manejo de errores
+        console.log(error);
+      }); 
+
+
+}
+
+
+
+
 
   function Agregaraqui(e) {
     console.log(Agregaraqui)
@@ -37,6 +219,7 @@ const App = () => {
 
   return (
     <div>
+      <button className='buton' onClick={() => { eliminarTodo() }}>eliminarTodo</button>
       <form
         onSubmit={evento => {
           setFrases(frases.concat(frase));
@@ -44,7 +227,7 @@ const App = () => {
           setFrase("");
         }}>
 
-        <input type="text" name="datos" value={frase} onChange={evento => { setFrase(evento.target.value) }} />
+        <input type="text" name="datos" value={frase} onChange={evento => { agregarFrases(evento) }} />
 
       </form>
 
@@ -53,7 +236,7 @@ const App = () => {
           <ul
             key={index}>
             {frase}
-            <button onClick={()=>{eliminarfrases(index)}}>
+            <button onClick={() => { eliminarfrases(index) }}>
               Anular
             </button>
 
